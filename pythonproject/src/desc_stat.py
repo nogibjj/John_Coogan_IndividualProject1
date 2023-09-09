ac5fcacdd1fb5ac0e6e958d6d89c772188d1f6a2
johncoogan53 <john.h.coogan@protonmail.com>
2023-09-08 20:11:51 -0400

    Updated the csv to a link

--- pythonproject/src/desc_stat.py
+++ pythonproject/src/desc_stat.py
@@ -1,6 +1,7 @@
+"""This is a script that will call the descriptive statistics function (describe()) from lib.py"""
 import lib
 
-data = lib.load_data_from_csv("pythonproject\src\data.csv")
+data = lib.load_data_from_csv("https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv")
 
 if data is not None:
     summary = lib.summary_statistics(data)
