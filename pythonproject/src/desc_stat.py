e510fc0443c27a148e1636b06cb1168a139a0b41
johncoogan53 <john.h.coogan@protonmail.com>
2023-09-08 21:59:59 -0400

    "changed script imports because thats an issue"

--- pythonproject/src/desc_stat.py
+++ pythonproject/src/desc_stat.py
@@ -1,10 +1,11 @@
 """This is a script that will call the descriptive statistics function from lib.py"""
-import lib
+from pythonproject.src.lib import load_data_from_csv
+from pythonproject.src.lib import summary_statistics
 
-data = lib.load_data_from_csv("https://www.statlearning.com/s/Auto.csv")
+data = load_data_from_csv("https://www.statlearning.com/s/Auto.csv")
 
 if data is not None:
-    summary = lib.summary_statistics(data)
+    summary = summary_statistics(data)
     print(data.head())
     print(summary)
     print(" ")
