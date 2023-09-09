8d74124d778aaba37fde34ae71ae954a60c3585f
johncoogan53 <john.h.coogan@protonmail.com>
2023-09-08 21:58:41 -0400

    import lib for both docs, changed to auto

--- pythonproject/src/desc_stat.py
+++ pythonproject/src/desc_stat.py
@@ -1,11 +1,10 @@
 """This is a script that will call the descriptive statistics function from lib.py"""
-from pythonproject.src.lib import load_data_from_csv
-from pythonproject.src.lib import summary_statistics
+import lib
 
-data = load_data_from_csv("https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv")
+data = lib.load_data_from_csv("https://www.statlearning.com/s/Auto.csv")
 
 if data is not None:
-    summary = summary_statistics(data)
+    summary = lib.summary_statistics(data)
     print(data.head())
     print(summary)
     print(" ")
