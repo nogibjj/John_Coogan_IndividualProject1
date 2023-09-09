db8e0191a175eea9073ff6d7bf7fa83f7755fe25
johncoogan53 <john.h.coogan@protonmail.com>
2023-09-08 21:07:48 -0400

    changed import to address nomodule for lib

--- pythonproject/src/desc_stat.py
+++ pythonproject/src/desc_stat.py
@@ -1,12 +1,10 @@
 """This is a script that will call the descriptive statistics function from lib.py"""
-import lib
+from src.lib import *
 
-data = lib.load_data_from_csv(
-    "https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv"
-)
+data = load_data_from_csv("https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv")
 
 if data is not None:
-    summary = lib.summary_statistics(data)
+    summary = summary_statistics(data)
     print(data.head())
     print(summary)
     print(" ")
