7a9f268a02658299877bf3947c464c6b5b2d96f6
johncoogan53 <141885470+johncoogan53@users.noreply.github.com>
2023-09-17 15:48:25 +0000

    mild changes still packaging issues

--- pythonproject/src/desc_stat.py
+++ pythonproject/src/desc_stat.py
@@ -1,6 +1,5 @@
 """This is a script that will call the descriptive statistics function from lib.py"""
-from pythonproject.src.lib import load_data_from_csv
-from pythonproject.src.lib import summary_statistics
+import lib
 import matplotlib.pyplot as plt
 
 
@@ -8,9 +7,7 @@ data = load_data_from_csv("https://www.statlearning.com/s/Auto.csv")
 
 if data is not None:
     summary = summary_statistics(data)
-    print(data.head())
     print(summary)
-    print(" ")
 
 
 def plot_visualize(dataframe):
